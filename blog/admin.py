f2e3a8e8da90a86ab1cd88e0d18dbaac33e95d51
Jacek Polański <kontakt@jpolanski.pl>
2015-08-20 18:20:13 +0200

    admin user added

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Topic, Post
+
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('subject', 'title')
+
+admin.site.register(Post, PostAdmin)
+admin.site.register(Topic)
