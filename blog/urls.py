dd504368875ac9c4f8323490b3ec53409d3c44ab
Jacek Polański <kontakt@jpolanski.pl>
2015-08-20 20:02:14 +0200

    [blog] added index and detail pages
    * urls
    * templates

---
+++ blog/urls.py
@@ -0,0 +1,10 @@
+from django.conf.urls import url
+
+from . import views
+
+urlpatterns = [
+    # ex: /blog/
+    url(r'^$', views.index, name='index'),
+    # ex: /blog/5/
+    url(r'^(?P<topic_id>[0-9]+)/$', views.detail, name='detail')
+]
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,17 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+from .models import Topic
 
-# Create your views here.
+
+def index(request):
+    latest_topics_list = Topic.objects.order_by('-date_added')[:5]
+
+    context = {
+        'latest_topics_list': latest_topics_list,
+    }
+
+    return render(request, 'blog/index.html', context)
+
+
+def detail(request, topic_id):
+    return HttpResponse("Topic: %s." % topic_id)
