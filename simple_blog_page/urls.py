a59e095b1454d7f1482ad7c211834f08095f1a53
Jacek Polański <kontakt@jpolanski.pl>
2015-08-21 17:16:13 +0200

    [blog] Namespace for urls

--- simple_blog_page/urls.py
+++ simple_blog_page/urls.py
@@ -17,6 +17,6 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^blog/', include('blog.urls')),
+    url(r'^blog/', include('blog.urls', namespace="blog")),
     url(r'^admin/', include(admin.site.urls)),
 ]
