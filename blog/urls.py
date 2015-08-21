a1df193f95a45053efc20096d04f23fd3563ed1e
Jacek Polański <kontakt@jpolanski.pl>
2015-08-21 18:18:43 +0200

    [blog] Added result page:
    * vote form in details view
    * new urls
    * removed useless latest_posts_list

--- blog/urls.py
+++ blog/urls.py
@@ -6,5 +6,9 @@ urlpatterns = [
     # ex: /blog/
     url(r'^$', views.index, name='index'),
     # ex: /blog/5/
-    url(r'^(?P<topic_id>[0-9]+)/$', views.detail, name='detail')
+    url(r'^(?P<topic_id>[0-9]+)/$', views.detail, name='detail'),
+    # ex: /blog/5/vote
+    url(r'^(?P<topic_id>[0-9]+)/vote/$', views.vote, name='vote'),
+    # ex: /blog/5/results
+    url(r'^(?P<topic_id>[0-9]+)/results/$', views.results, name='results'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,6 @@
+from django.core.urlresolvers import reverse
 from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponseRedirect
 from .models import Topic, Post
 
 
@@ -14,11 +16,32 @@ def index(request):
 
 def detail(request, topic_id):
     topic = get_object_or_404(Topic, pk = topic_id)
-    latest_posts_list = Post.objects.filter(topic = topic_id)
 
     context = {
         'topic': topic,
-        'latest_posts_list': latest_posts_list,
     }
 
     return render(request, 'blog/detail.html', context)
+
+
+def vote(request, topic_id):
+    topic = get_object_or_404(Topic, pk=topic_id)
+
+    try:
+        selected_post = topic.post_set.get(pk=request.POST['post'])
+    except (KeyError, Post.DoesNotExist):
+        return render(request, 'blog/detail.html', {
+            'topic': topic,
+            'error_message': "Nie zaznaczyłeś posta",
+        })
+    else:
+        selected_post.votes += 1
+        selected_post.save()
+
+    return HttpResponseRedirect(reverse('blog:results', args=(topic.id,)))
+
+
+def results(request, topic_id):
+    topic = get_object_or_404(Topic, pk = topic_id)
+
+    return render(request, 'blog/result.html', {'topic': topic})
