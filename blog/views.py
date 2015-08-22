92299fadccb10121e5d6fc5087accff4bde1a39f
Jacek Polański <kontakt@jpolanski.pl>
2015-08-23 01:10:18 +0200

    [blog] Generic views

--- blog/views.py
+++ blog/views.py
@@ -1,27 +1,28 @@
 from django.core.urlresolvers import reverse
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponseRedirect
+from django.views import generic
+
 from .models import Topic, Post
 
 
-def index(request):
-    latest_topics_list = Topic.objects.order_by('-date_added')[:5]
+class IndexView(generic.ListView):
+    template_name = 'blog/index.html'
+    context_object_name = 'latest_topics_list'
 
-    context = {
-        'latest_topics_list': latest_topics_list,
-    }
-
-    return render(request, 'blog/index.html', context)
+    def get_queryset(self):
+        """Return the last five published topics"""
+        return Topic.objects.order_by('-date_added')[:5]
 
 
-def detail(request, topic_id):
-    topic = get_object_or_404(Topic, pk = topic_id)
+class DetailView(generic.DetailView):
+    model = Topic
+    template_name = 'blog/detail.html'
 
-    context = {
-        'topic': topic,
-    }
 
-    return render(request, 'blog/detail.html', context)
+class ResultsView(generic.DetailView):
+    model = Topic
+    template_name = 'blog/result.html'
 
 
 def vote(request, topic_id):
@@ -39,9 +40,3 @@ def vote(request, topic_id):
         selected_post.save()
 
     return HttpResponseRedirect(reverse('blog:results', args=(topic.id,)))
-
-
-def results(request, topic_id):
-    topic = get_object_or_404(Topic, pk = topic_id)
-
-    return render(request, 'blog/result.html', {'topic': topic})
